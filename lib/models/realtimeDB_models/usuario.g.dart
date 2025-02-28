631c092d9e04175b7a5aff42f833d891522db63f
Geo290 <jorgeemmanuel.2003@gmail.com>
2025-02-28 10:35:44 -0600

    Refactor: file rebuilt

--- lib/models/realtimeDB_models/usuario.g.dart
+++ lib/models/realtimeDB_models/usuario.g.dart
@@ -10,12 +10,12 @@ Usuario _$UsuarioFromJson(Map<String, dynamic> json) => Usuario(
       idUsuario: (json['idUsuario'] as num).toInt(),
       nombre: json['nombre'] as String,
       apellidos: json['apellidos'] as String,
-      telefono: json['telefono'] != null ? (json['telefono'] as num).toInt() : null,
+      telefono: (json['telefono'] as num?)?.toInt(),
       correo: json['correo'] as String,
       contrasena: json['contrasena'] as String,
       rol: $enumDecode(_$RolEnumMap, json['rol']),
       activo: json['activo'] as bool,
-      idVehiculo: json['idVehiculo'] != null ? (json['idVehiculo'] as num).toInt() : null,
+      idVehiculo: (json['idVehiculo'] as num?)?.toInt(),
     );
 
 Map<String, dynamic> _$UsuarioToJson(Usuario instance) => <String, dynamic>{
@@ -27,7 +27,7 @@ Map<String, dynamic> _$UsuarioToJson(Usuario instance) => <String, dynamic>{
       'contrasena': instance.contrasena,
       'rol': _$RolEnumMap[instance.rol]!,
       'activo': instance.activo,
-      'idVehiculo': instance.idVehiculo
+      'idVehiculo': instance.idVehiculo,
     };
 
 const _$RolEnumMap = {
