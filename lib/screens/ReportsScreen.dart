b8a35c34d52c11d1c159604279a76cade6999485
Geo290 <jorgeemmanuel.2003@gmail.com>
2025-03-24 20:27:51 -0600

    Refactor & Fix: Ya genera reportes; los filtros fueron reparados

--- lib/screens/ReportsScreen.dart
+++ lib/screens/ReportsScreen.dart
@@ -1,15 +1,18 @@
+import 'package:padillaroutea/services/realtime_db_services/realtime_db_helper.dart';
+import 'package:padillaroutea/services/realtime_db_services/incidentes_helper.dart';
+import 'package:padillaroutea/models/realtimeDB_models/incidente_registro.dart';
+import 'package:padillaroutea/services/file_generator/excel_generator.dart';
+import 'package:padillaroutea/models/realtimeDB_models/viaje_registro.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:padillaroutea/models/realtimeDB_models/usuario.dart';
+import 'dart:io';
 
 class ReportsScreen extends StatefulWidget {
-  final Usuario usuario;
-
-  ReportsScreen({required this.usuario});
-
+  const ReportsScreen({super.key, required usuario});
   @override
   _ReportsScreenState createState() => _ReportsScreenState();
 }
-
 
 class _ReportsScreenState extends State<ReportsScreen> {
   String _selectedFilter = 'Día';
@@ -18,13 +21,17 @@ class _ReportsScreenState extends State<ReportsScreen> {
   DateTime? _selectedDay;
   int? _selectedMonth;
 
-  Future<void> _selectDate(BuildContext context, bool isStartDate) async {
+  late RealtimeDbHelper database;
+  late DatabaseReference ref;
+
+  Future<DateTime> _selectDate(BuildContext context, bool isStartDate) async {
     final DateTime? picked = await showDatePicker(
       context: context,
       initialDate: DateTime.now(),
       firstDate: DateTime(2023),
       lastDate: DateTime(2101),
     );
+
     if (picked != null) {
       setState(() {
         if (_selectedFilter == 'Día') {
@@ -35,13 +42,75 @@ class _ReportsScreenState extends State<ReportsScreen> {
           _endDate = picked;
         }
       });
+      print(picked);
+      return picked;
+      
+    } else {
+      throw Exception("No date selected");
+    }
+
+  }
+
+  Future<bool> _requestStoragePermission() async {
+    if (Platform.isAndroid) {
+      if (await Permission.storage.request().isGranted) {
+        return true;
+      }
+      if (await Permission.manageExternalStorage.request().isGranted) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  Future<List<IncidenteRegistro>> getIncidentes(int year) {
+    database = RealtimeDbHelper();
+    IncidentesHelper incidentesHelper = IncidentesHelper(database);
+
+    switch (_selectedFilter) {
+      case 'Día':
+        return incidentesHelper.fetchByDate(_selectedDay!);
+
+      case 'Semana':
+        return incidentesHelper.fetchByDateRange(_startDate!, _endDate!);
+
+      case 'Mes':
+        return incidentesHelper.fetchByMonth(year, _selectedMonth!);
+
+      default:
+        return Future.value([]);
     }
   }
 
-  void _generateReport() {
-    ScaffoldMessenger.of(context).showSnackBar(
-      SnackBar(content: Text('📄 Generando reporte...')),
-    );
+  List<String> getColumnNames(String collection) {
+    if (collection == "incidentes_registro") {
+      return IncidenteRegistro.getKeys();
+    } else if (collection == "viajes_registro") {
+      return ViajeRegistro.getKeys();
+    } else {
+      throw Exception("No se encontró la colección: $collection");
+    }
+  }
+
+  void _generateReport() async {
+    // SOLICITAR PERMISO DE ALMACENAMIENTO
+    if (!(await _requestStoragePermission())) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+            content: Text('PERMISO DENEGADO. NO SE PUEDE GUARDAR EL ARCHIVO')),
+      );
+    }
+
+    int year = 2025;
+    int month = _selectedMonth ?? 1;
+    List<String> columnNames = getColumnNames("incidentes_registro");
+    List<IncidenteRegistro> registroSet = await getIncidentes(2025);
+
+    if (registroSet.isEmpty) {
+      print("No se hayo nada");
+      return;
+    }
+    generateFile(registroSet, columnNames, "excelTest.xlsx");
   }
 
   Widget _buildFilterInputs() {
@@ -49,40 +118,40 @@ class _ReportsScreenState extends State<ReportsScreen> {
       case 'Día':
         return Column(
           children: [
-            Text("Selecciona un día"),
-            SizedBox(height: 8),
+            const Text("Selecciona un día"),
+            const SizedBox(height: 8),
             ElevatedButton(
               onPressed: () => _selectDate(context, true),
               child: Text(_selectedDay == null
                   ? 'Elegir día'
-                  : '📅 ${_selectedDay!.toLocal()}'.split(' ')[0]),
+                  : '📅 ${_selectedDay!.toLocal()}'.split('-')[2].split(' ')[0]),
             ),
           ],
         );
       case 'Semana':
         return Column(
           children: [
-            Text("Selecciona intervalo de fechas"),
-            SizedBox(height: 8),
+            const Text("Selecciona intervalo de fechas"),
+            const SizedBox(height: 8),
             ElevatedButton(
               onPressed: () => _selectDate(context, true),
               child: Text(_startDate == null
                   ? 'Inicio de semana'
-                  : '📅 ${_startDate!.toLocal()}'.split(' ')[0]),
+                  : '📅 ${_startDate!.toLocal()}'.split('-')[2].split(' ')[0]),
             ),
-            SizedBox(height: 8),
+            const SizedBox(height: 8),
             ElevatedButton(
               onPressed: () => _selectDate(context, false),
               child: Text(_endDate == null
                   ? 'Fin de semana'
-                  : '📅 ${_endDate!.toLocal()}'.split(' ')[0]),
+                  : '📅 ${_endDate!.toLocal()}'.split('-')[2].split(' ')[0]),
             ),
           ],
         );
       case 'Mes':
         return DropdownButtonFormField<int>(
           value: _selectedMonth,
-          decoration: InputDecoration(labelText: "Selecciona un mes"),
+          decoration: const InputDecoration(labelText: "Selecciona un mes"),
           items: List.generate(12, (index) {
             return DropdownMenuItem<int>(
               value: index + 1,
@@ -104,8 +173,18 @@ class _ReportsScreenState extends State<ReportsScreen> {
 
   String _monthName(int month) {
     const List<String> months = [
-      'Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio',
-      'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'
+      'Enero',
+      'Febrero',
+      'Marzo',
+      'Abril',
+      'Mayo',
+      'Junio',
+      'Julio',
+      'Agosto',
+      'Septiembre',
+      'Octubre',
+      'Noviembre',
+      'Diciembre'
     ];
     return months[month - 1];
   }
@@ -114,18 +193,18 @@ class _ReportsScreenState extends State<ReportsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('📊 Generar Reportes'),
+        title: const Text('📊 Generar Reportes'),
         backgroundColor: Colors.blueAccent,
       ),
       body: Padding(
         padding: const EdgeInsets.all(20.0),
         child: Column(
           children: [
-            Text(
+            const Text(
               "Selecciona el tipo de filtro:",
               style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
             ),
-            SizedBox(height: 10),
+            const SizedBox(height: 10),
             ToggleButtons(
               isSelected: ['Día', 'Semana', 'Mes']
                   .map((e) => _selectedFilter == e)
@@ -151,12 +230,12 @@ class _ReportsScreenState extends State<ReportsScreen> {
             _buildFilterInputs(),
             const SizedBox(height: 30),
             ElevatedButton.icon(
-              onPressed: _generateReport,
-              icon: Icon(Icons.picture_as_pdf),
-              label: Text('Generar Reporte'),
+              onPressed: () => _generateReport(),
+              icon: const Icon(Icons.picture_as_pdf),
+              label: const Text('Generar Reporte'),
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.green,
-                minimumSize: Size(double.infinity, 50),
+                minimumSize: const Size(double.infinity, 50),
               ),
             ),
           ],
