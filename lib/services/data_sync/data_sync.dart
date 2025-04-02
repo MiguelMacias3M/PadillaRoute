da6db43cf47001a3f2481c778ab1f414a4984f78
Geo290 <jorgeemmanuel.2003@gmail.com>
2025-04-01 18:45:40 -0600

    Feature: syncronization data

---
+++ lib/services/data_sync/data_sync.dart
@@ -0,0 +1,53 @@
+import 'package:padillaroutea/services/objectbox_services/viajes_registro_helper.dart';
+import 'package:padillaroutea/services/realtime_db_services/viajes_helper.dart';
+import 'package:padillaroutea/models/realtimeDB_models/viaje_registro.dart'
+    as viajeRegistroRealTime;
+import 'package:padillaroutea/models/objectBox_models/viaje_registro.dart';
+import 'dart:async';
+
+class DataSync {
+  final ViajesHelper _realTimeViajesHelper;
+  final ViajesRegistroHelper _objectBox;
+  Timer? _timer;
+
+  DataSync(this._objectBox, this._realTimeViajesHelper);
+
+  void startTimer() {
+    _timer = Timer.periodic(const Duration(milliseconds: 300000), (timer) {
+      if (checkForPendingSyncronization()) {
+        syncDat("111133, 131133");
+      }
+    });
+  }
+
+  void stopTimer() {
+    _timer?.cancel();
+  }
+  bool checkForPendingSyncronization() {
+    final List<ViajeRegistro> registros = _objectBox.getAllRegistros();
+    return registros.isNotEmpty;
+  }
+
+  void syncDat(String coordenadas) {
+    final List<ViajeRegistro> registros = _objectBox.getAllRegistros();
+    final objItem = registros[0];
+
+    final castedItem = viajeRegistroRealTime.ViajeRegistro(
+        idRegistro: objItem.id,
+        idRuta: objItem.idRuta,
+        idVehiculo: objItem.idVehiculo,
+        idUsuario: objItem.idChofer,
+        paradasRegistro: objItem.paradasMap,
+        horaInicio: objItem.horaInicio,
+        horaFinal: objItem.horaFinal,
+        tiempoTotal: objItem.tiempoTotal,
+        totalPasajeros: objItem.totalPasajeros,
+        distanciaRecorrida: objItem.distanciaRecorrida.toInt(),
+        velocidadPromedio: objItem.velocidadPromedio.toInt(),
+        coordenadas: coordenadas);
+
+    _realTimeViajesHelper.setNew(castedItem).then((value) {
+      _objectBox.deleteRegistro();
+    });
+  }
+}
